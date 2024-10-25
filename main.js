e44042bd889fbcb24ea625d8ef8d7a9f284c29a9
Devin Coleman <croilders48@gmail.com>
2024-10-25 15:44:49 -0700

    title are being added in depending on the list

--- main.js
+++ main.js
@@ -17,14 +17,33 @@ import z from '/api.js'
   fetch('https://api.themoviedb.org/3/trending/movie/week?language=en-US', options)
   .then(res => res.json())
   .then(res => {
-    // console.log(res.results[0])
-    // console.log(res.original_title)
-   
+    console.log(res.results)
+     
+    // function createButton(buttonText, buttonId) {
+    //     const button = document.createElement('button'); // Create a button element
+    //     button.textContent = buttonText; // Set the button text
+    //     button.id = buttonId; // Set the button ID (or use setAttribute)
+ 
+     res.results.forEach((x, i) => {
+       
+       let newHeading = document.createElement("h1") 
+        newHeading.textContent = res.results[i].title
+        document.body.appendChild(newHeading)
+
+        
+        
+        // i was trying to add a button each time a new title was added need to look into this
+
+        
+
+     })
+
+
     const baseURL = "https://image.tmdb.org/t/p/w500";
     
-    document.querySelector('h1').innerText = res.results[0].original_title
-    document.querySelector('h2').innerText = res.results[0].overview
-    document.querySelector('img').src = baseURL + res.results[0].poster_path
+    // document.querySelector('h1').innerText = res.results[0].original_title
+    // document.querySelector('h2').innerText = res.results[0].overview
+    // document.querySelector('img').src = baseURL + res.results[0].poster_path
 
 
 
